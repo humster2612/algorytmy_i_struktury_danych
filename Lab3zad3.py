aea4b017994d153bd7f978605cc132694e43272d
Victoria <w67964@student.wsiz.edu.pl>
2023-04-11 13:52:42 +0200

    Made lab3zad3

---
+++ Lab3zad3.py
@@ -0,0 +1,16 @@
+a=int(input("Podaj a : "))
+b=int(input("Podaj b : "))
+if b==0:
+    print("NWD jest: ",a)
+
+elif a or b<0:
+    print("Nie może tak być! ")
+
+else: 
+
+    while b>0:
+        reszta = a%b
+        a=b
+        b=reszta
+    print("NWD jest: ",a )
+    
