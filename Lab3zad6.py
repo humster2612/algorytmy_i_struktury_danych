5fb061188660d7d3ffee06d4a934b04f986c6406
humster2612 <129902401+humster2612@users.noreply.github.com>
2023-04-23 23:32:41 +0200

    Update Lab3zad6.py

--- Lab3zad6.py
+++ Lab3zad6.py
@@ -1,7 +1,11 @@
-def wynik(i):
-    if i < 3:
+def dec2bin(num):
+    if num == 0:
+        return 0
+    elif num == 1:
         return 1
-    elif i % 2 == 0:
-        return wynik(i-3) + wynik(i-1) + 1
     else:
-        return wynik(i-1) % 7
+        result = dec2bin(num // 2)
+        result = result * 10 + num % 2
+        print(result)
+        return result
+dec2bin(103)
