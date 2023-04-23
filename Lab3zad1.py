3d0969910c252f9232098e9b8e5d3146b005273d
humster2612 <129902401+humster2612@users.noreply.github.com>
2023-04-23 09:35:14 +0200

    Update Lab3zad1.py

--- Lab3zad1.py
+++ Lab3zad1.py
@@ -1,13 +1,6 @@
 def sil(a):
-    if(a==0):
-        return 1
-    else:
-        return sil(a-1)*a
-    
-
-print("Podaj liczbę:")
-
-b=int(input())
-
-print(sil(b))
-
+    if (a == 0):
+        return 1 
+   else:
+        return sil(a - 1) * a
+print(sil(int(input("Podaj liczbę:"))))
