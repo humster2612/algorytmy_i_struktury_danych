bfe775f287fbea39b3c6d08f869b61af2e4ec147
humster2612 <129902401+humster2612@users.noreply.github.com>
2023-04-23 09:43:40 +0200

    Update Lab3zad6.py

--- Lab3zad6.py
+++ Lab3zad6.py
@@ -1,13 +1,7 @@
 def wynik(i):
-    if i<3:
+    if i < 3:
         return 1
+    elif i % 2 == 0:
+        return wynik(i-3) + wynik(i-1) + 1
     else:
-        if i%2==0:
-            return wynik(i-3)+wynik(i-1)+1
-        
-        else:
-            return wynik(i-1)%7
-        
-i=int(input("Podaj i:"))
-
-print(wynik(i))
+        return wynik(i-1) % 7
