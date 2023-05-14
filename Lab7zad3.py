39f4e52b90ce3ed9dd7f64e0d3569294d6f90f28
humster2612 <129902401+humster2612@users.noreply.github.com>
2023-05-14 20:56:28 +0200

    Create Lab7zad3.py

---
+++ Lab7zad3.py
@@ -0,0 +1,18 @@
+def parChecker(symbolString):
+    s = Stack()
+    balanced = True
+    index = 0
+    while index < len(symbolString) and balanced:
+        symbol = symbolString[index]
+        if symbol in '({[<':
+            s.push(symbol)
+        else:
+            if s.isEmpty():
+                balanced = False
+            else:
+                s.pop()
+        index += 1
+    if balanced and s.isEmpty():
+        return True
+    else:
+        return False
