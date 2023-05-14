b9c6ead2761fb14b35a00eca89f5ed260fddb920
humster2612 <129902401+humster2612@users.noreply.github.com>
2023-05-14 21:13:54 +0200

    Update Lab7zad2(kolejka).py

--- Lab7zad2(kolejka).py
+++ Lab7zad2(kolejka).py
@@ -1,15 +1,15 @@
-def hot_potato(num):
+def kod(num):
     register = Queue()
-    players = ["John", "Lusi", "Gor", "Mrok", "Arnold", "Nora"]
-    for player in players:
-        register.enqueue(player)
+    ludzi= ["Oleg", "Matwij", "Kola", "Ania", "Olha", "Misza"]
+    for lud in ludzi:
+        register.enqueue(lud)
 
     while register.size() > 1:
         for i in range(num):
             register.enqueue(register.dequeue())
 
-        numbers = rd.randint(0, 35)
-        if numbers == 5:
+        liczby= rd.randint(0, 35)
+        if liczby == 5:
             register.dequeue()
 
 
