c9b38a3d702010bf8461dcdebf1d3303297b8757
humster2612 <129902401+humster2612@users.noreply.github.com>
2023-05-14 20:50:12 +0200

    Create Lab7zad4.py

---
+++ Lab7zad4.py
@@ -0,0 +1,30 @@
+def loik(ozn):
+    operand_stak = Stack()
+    token_list = ozn.split()
+    for token in token_list:
+        if token in "0123456789":
+            operand_stak.push(float(token))
+
+        else:
+            operand2 = operand_stak.pop()
+            operand1 = operand_stak.pop()
+            result = math(token, operand1, operand2)
+            operand_stak.push(result)
+        return operand_stak.pop()
+
+a = loik("7 8 + 9 11 - 90 *")
+print(a)
+
+
+
+def math(operator,operan1,operan2):
+    if operator == '':
+        return operan1  operan2
+    elif operator == '*':
+        return operan1 * operan2
+    elif operator == '/':
+        return operan1 / operan2
+    elif operator == '+':
+        return operan1 + operan2
+    elif operator == '-':
+        return operan1 - operan2
